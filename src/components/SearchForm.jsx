9d30319b1f8d505b93b182c3c9c0e98793c98bbd
NaraeJang <j.naaaarae@gmail.com>
2023-10-25 11:02:37 -0400

    SearchForm - Structure

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,24 @@
+import { Form, useNavigation } from "react-router-dom";
+import Wrapper from "../assets/wrappers/SearchForm";
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue="vodka"
+        />
+        <button type="submit" disabled={isSubmitting} className="btn">
+          {isSubmitting ? "searching" : "search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 export default SearchForm;
