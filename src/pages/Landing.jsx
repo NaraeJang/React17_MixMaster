8c1e3d5509a8bed6308465732775c154d4c70b7f
NaraeJang <j.naaaarae@gmail.com>
2023-10-25 16:02:28 -0400

    react query - complete

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,18 +2,37 @@ import axios from "axios";
 import { useLoaderData } from "react-router-dom";
 import SearchForm from "../components/SearchForm";
 import CocktailList from "../components/CocktailList";
+import { useQuery } from "@tanstack/react-query";
 
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async ({ request }) => {
-  const url = new URL(request.url);
-  const searchTerm = url.searchParams.get("search") || "";
-  const response = await axios.get(`${baseURL}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm };
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || ""],
+    queryFn: async () => {
+      const response = await axios.get(`${baseURL}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
 };
 
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get("search") || "";
+    const test = await queryClient.ensureQueryData(
+      searchCocktailsQuery(searchTerm)
+    );
+    console.log(test);
+
+    // return { drinks: response.data.drinks, searchTerm };
+    return { searchTerm };
+  };
+
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
 
   return (
     <>
